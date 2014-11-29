d45f18833e46b0c4f14ba0e6511d8d50239fb315
Mariusz Pluciński <mplucinski@mplucinski.com>
2014-11-29 23:28:45 +0100

    Remove wrong test from TestBuilder

--- __init__.py
+++ __init__.py
@@ -80,27 +80,9 @@ class MockTarget(Target):
 		self.build_called = True
 
 class TestBuilder(unittest.TestCase):
-	def build(self, targets={'foo', 'bar', 'baz', 'qux'}, dependencies={'foo': ['bar', 'baz'], 'baz': ['qux']}, args=None):
-		build = Build()
-		targets = { i: MockTarget(i) for i in targets }
-		for name, target in targets.items():
-			target.dependencies = { targets[dep] for dep in dependencies[name] } if name in dependencies else {}
-		targets = set(targets.values())
-		build.targets |= targets
-		build(args=args)
-		return build, targets
-
 	def test_single_target(self):
-		build, targets = self.build(targets={'foo'}, dependencies={})
-		targets = iter(targets)
-		self.assertEqual(next(targets).build_called, True)
-
-	def test_dependencies(self):
-		build, targets = self.build(targets={'foo', 'bar', 'baz', 'qux'},
-			dependencies={'foo': ['bar', 'baz'], 'baz': ['qux']}
-		)
-		i_target = iter(targets)
-		self.assertEqual(next(i_target).build_called, True)
-		self.assertEqual(next(i_target).build_called, True)
-		self.assertEqual(next(i_target).build_called, True)
-		self.assertEqual(next(i_target).build_called, True)
+		build = Build()
+		foo = MockTarget('foo')
+		build.targets |= {foo}
+		build()
+		self.assertEqual(True, foo.build_called)
