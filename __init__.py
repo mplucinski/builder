522182f3539c8817460a48583c78023a2bdabbb2
Mariusz Pluciński <mplucinski@mplucinski.com>
2014-11-29 20:31:37 +0100

    Add logger support and -v/--verbose option

--- __init__.py
+++ __init__.py
@@ -2,6 +2,32 @@
 # -*- coding: utf-8 -*-
 
 import argparse
+import logging
+import sys
+
+def _init_logger(verbosity):
+	def verbosity_to_level(verbosity):
+		if verbosity == 0: return logging.WARNING
+		elif verbosity == 1: return logging.INFO
+		elif verbosity == 2: return logging.DEBUG
+		elif verbosity >= 3:
+			return logging.DEBUG - verbosity + 2
+
+	class Formatter(logging.Formatter):
+		def format(self, record):
+			def level_fmt(level):
+				if level == logging.CRITICAL: return '!!! {msg}'
+				elif level == logging.ERROR: return '! {msg}'
+				elif level == logging.WARNING: return '> {msg}'
+				elif level == logging.INFO: return '- {msg}'
+				elif level <= logging.DEBUG: return '--' + '-'*(logging.DEBUG - level) + ' {msg}'
+			return level_fmt(record.levelno).format(msg=record.msg)
+
+	handler = logging.StreamHandler(sys.stdout)
+	handler.setFormatter(Formatter())
+	logging.getLogger().setLevel(verbosity_to_level(verbosity))
+	logging.getLogger().addHandler(handler)
+	logging.debug('Logger configured')
 
 class Target:
 	def __init__(self, name):
@@ -13,6 +39,8 @@ class Target:
 class Build:
 	def _arguments_parser(self):
 		parser = argparse.ArgumentParser(description='Builder - Integration-centered build system')
+		parser.add_argument('-v', '--verbose', action='count',
+				help='Verbose output')
 		parser.add_argument('target', nargs='*', type=str, metavar='TARGET',
 				help='Target(s) to build (all, if nothing passed)')
 		return parser
@@ -23,6 +51,8 @@ class Build:
 	def __call__(self, args=None):
 		parser = self._arguments_parser()
 		args = parser.parse_args(args)
+
+		_init_logger(args.verbose)
 
 		targets = self.targets
 		if args.target:
