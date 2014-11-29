6ab61aba0aa3e74a9ae49be01a0a1743716ab8a3
Mariusz Pluciński <mplucinski@mplucinski.com>
2014-11-29 20:27:49 +0100

    Processing main targets

--- __init__.py
+++ __init__.py
@@ -1,2 +1,37 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
+import argparse
+
+class Target:
+	def __init__(self, name):
+		self.name = name
+
+	def _build(self):
+		self.build()
+
+class Build:
+	def _arguments_parser(self):
+		parser = argparse.ArgumentParser(description='Builder - Integration-centered build system')
+		parser.add_argument('target', nargs='*', type=str, metavar='TARGET',
+				help='Target(s) to build (all, if nothing passed)')
+		return parser
+
+	def __init__(self):
+		self.targets = set()
+
+	def __call__(self, args=None):
+		parser = self._arguments_parser()
+		args = parser.parse_args(args)
+
+		targets = self.targets
+		if args.target:
+			targets = []
+			for i in args.target:
+				try:
+					targets.append(next(j for j in self.targets if j.name == i))
+				except StopIteration as e:
+					raise Exception('Global target "{}" not found'.format(i)) from e
+
+		for target in targets:
+			target._build()
--- tests/test.py
+++ tests/test.py
@@ -9,5 +9,26 @@ sys.path.append(str((pathlib.Path(__file__).parent / '..' / '..').resolve()))
 
 import builder
 
+class MockTarget(builder.Target):
+	def __init__(self, *args, **kwargs):
+		super().__init__(*args, **kwargs)
+		self.build_called = False
+
+	def build(self):
+		self.build_called = True
+
+class TestBuilder(unittest.TestCase):
+	def build(self, targets=set()):
+		build = builder.Build()
+		targets = { MockTarget(i) for i in targets }
+		build.targets |= targets
+		build()
+		return build, targets
+
+	def test_single_target(self):
+		build, targets = self.build(targets={'foo'})
+		targets = iter(targets)
+		self.assertEqual(next(targets).build_called, True)
+
 if __name__ == '__main__':
 	unittest.main()
