4e9bcd0f934cc7eec390e9f382efc089fe1810bf
Mariusz Pluciński <mplucinski@mplucinski.com>
2014-11-30 14:44:54 +0100

    Add Patch target

--- targets.py
+++ targets.py
@@ -14,6 +14,7 @@ from .base import samefile
 from .base import Scope
 from .base import Target
 from .config import MockConfig
+from .process import Process
 
 class Download(Target):
 	local_config_keys = {'url'}
@@ -52,6 +53,17 @@ class Extract(Target):
 
 		self.log(logging.INFO, 'extracting {}...'.format(file_input))
 		shutil.unpack_archive(file_input, str(target_dir))
+
+class Patch(Target):
+	local_config_keys = {'file', 'directory', 'strip'}
+	local_config_defaults = {'strip': 1}
+
+	def build(self, config):
+		patch = Process(
+			['patch', '-p{}'.format(config['strip']), '-i', str(config['file'])],
+			cwd=str(config['directory'])
+		)
+		patch.communicate()
 
 class TestDownload(unittest.TestCase):
 	def test_download(self):
@@ -94,9 +106,7 @@ class TestExtract(unittest.TestCase):
 				'file.input': archive_file,
 				'directory.output': temp_dir_out_path
 		})
-		config = MockConfig(Target.GlobalTargetLevel, {
-
-		})
+		config = MockConfig(Target.GlobalTargetLevel, {})
 		extract._build(config)
 
 		self.assertEqualDirectories(this_directory, temp_dir_out_path)
@@ -104,10 +114,71 @@ class TestExtract(unittest.TestCase):
 		temp_dir.cleanup()
 		temp_dir_out.cleanup()
 
+class TestPatch(unittest.TestCase):
+	input_file = '''YODA: Code!  Yes.  A programmer's strength flows from code
+      maintainability.  But beware of Perl.  Terse syntax... more
+      than one way to do it...  default variables.  The dark side
+      of code maintainability are they.  Easily they flow, quick
+      to join you when code you write.  If once you start down the
+      dark path, forever will it dominate your destiny, consume
+      you it will.
+LUKE: Is Perl better than Python?
+YODA: Yes... yes... yes.  Quicker, easier, more maintainable.
+LUKE: But how will I know why Python is better than Perl?
+YODA: You will know.  When your code you try to read six months
+      from now.'''
+
+	patch = '''--- a/{file_name}\t
++++ b/{file_name}\t
+@@ -7,5 +7,5 @@
+       you it will.
+ LUKE: Is Perl better than Python?
+-YODA: Yes... yes... yes.  Quicker, easier, more maintainable.
++YODA: No... no... no.  Quicker, easier, more seductive.
+ LUKE: But how will I know why Python is better than Perl?
+ YODA: You will know.  When your code you try to read six months
+'''
+
+	output_file = '''YODA: Code!  Yes.  A programmer's strength flows from code
+      maintainability.  But beware of Perl.  Terse syntax... more
+      than one way to do it...  default variables.  The dark side
+      of code maintainability are they.  Easily they flow, quick
+      to join you when code you write.  If once you start down the
+      dark path, forever will it dominate your destiny, consume
+      you it will.
+LUKE: Is Perl better than Python?
+YODA: No... no... no.  Quicker, easier, more seductive.
+LUKE: But how will I know why Python is better than Perl?
+YODA: You will know.  When your code you try to read six months
+      from now.'''
+
+	def test_patch(self):
+#		logging.basicCo
+
+		temp_dir = tempfile.TemporaryDirectory()
+		temp = pathlib.Path(temp_dir.name)
+		input_file = temp/'The Empire Strikes Back.txt'
+		input_file.open('w').write(self.input_file)
+		patch_file = temp/'The Empire Strikes Back.patch'
+		patch_file.open('w').write(self.patch.format(file_name=input_file.name))
+
+		patch = Patch('patch_files', **{
+			'directory': temp,
+			'file': patch_file
+		})
+		config = MockConfig(Target.GlobalTargetLevel, {})
+		patch._build(config)
+
+		output = input_file.open().read()
+		self.assertEqual(self.output_file, output)
+		temp_dir.cleanup()
+
+
 def load_tests(loader, tests, pattern):
 	suite = unittest.TestSuite()
 	suite.addTests(loader.loadTestsFromTestCase(TestDownload))
 	suite.addTests(loader.loadTestsFromTestCase(TestExtract))
+	suite.addTests(loader.loadTestsFromTestCase(TestPatch))
 	return suite
 
 if __name__ == '__main__':
