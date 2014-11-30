23ece5005492ef4ef21f794d5d7007fba0af17b8
Mariusz Pluciński <mplucinski@mplucinski.com>
2014-11-30 09:11:28 +0100

    Fix runtime tests directory

--- tests.py
+++ tests.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import pathlib
 import sys
 import unittest
 
 if __name__ == '__main__':
-	suite = unittest.defaultTestLoader.discover(start_dir='.', pattern='*.py', top_level_dir='..')
+	directory = pathlib.Path(__file__).parent
+	suite = unittest.defaultTestLoader.discover(start_dir=str(directory), pattern='*.py', top_level_dir=str(directory/'..'))
 	runner = unittest.TextTestRunner(verbosity=2 if '-v' in sys.argv else 1)
 	runner.run(suite)
