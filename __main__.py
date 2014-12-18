ede834bc148f1211344f563e1fe7c6a8dab01593
Mariusz Pluciński <mplucinski@mplucinski.com>
2014-12-19 00:26:56 +0100

    Raise exception if any test(s) has failed

--- __main__.py
+++ __main__.py
@@ -6,4 +6,6 @@ if __name__ == '__main__':
 	directory = pathlib.Path(__file__).parent
 	suite = unittest.defaultTestLoader.discover(start_dir=str(directory), pattern='*.py', top_level_dir=str(directory/'..'))
 	runner = unittest.TextTestRunner(verbosity=2 if '-v' in sys.argv else 1)
-	runner.run(suite)
+	result = runner.run(suite)
+	if not result.wasSuccessful():
+		raise Exception('Not all tests succeeded')
